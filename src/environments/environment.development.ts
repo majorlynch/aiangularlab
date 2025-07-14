7e70bc1ae33e9ee6642794fff258dcb1e3ce9c57
Conor Lynch <majorlynch@gmail.com>
2025-07-14 19:32:40 +0100

    added icon

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiKeyUrl: 'https://madebyconor.com/ailab/apikey',
+    apiKeyUrl: 'https://madebyconor.com/ailab/apikey/',
     featureFlags: {
       Gemini: true,
       Deepseek: true,
--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiKeyUrl: 'https://madebyconor.com/ailab/apikey',
+    apiKeyUrl: 'https://madebyconor.com/ailab/apikey/',
     featureFlags: {
       Gemini: true,
       Deepseek: true,
