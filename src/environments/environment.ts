f407bfb84c4d01656987e5783ba18d2d34c7c0b8
Conor Lynch <majorlynch@gmail.com>
2025-07-28 17:04:35 +0100

    added accordian

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
-//const baseUrl= 'https://madebyconor.com/';
-const baseUrl= 'http://localhost:3000/';
+const baseUrl= 'https://madebyconor.com/';
+//const baseUrl= 'http://localhost:3000/';
 
 export const environment = {
     production: false,
     baseUrl: baseUrl,
-    apiKeyUrl: baseUrl + 's1/apikey/',
-    apiUrlChatGPTChat: baseUrl + 's1/chatgptchat',
-    apiUrlChatGPTPrompt: baseUrl + 's1/chatgptprompt',
-    apiUrlChatGPTGenImage: baseUrl + 's1/chatgptgenimage',
+    apiKeyUrl: baseUrl + 'n1/apikey/',
+    apiUrlChatGPTChat: baseUrl + 'n1/chatgptchat',
+    apiUrlChatGPTPrompt: baseUrl + 'n1/chatgptprompt',
+    apiUrlChatGPTGenImage: baseUrl + 'n1/chatgptgenimage',
     featureFlags: {
       Gemini: true,
       Deepseek: true,
